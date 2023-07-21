92d875609903deea556440cbafd5943973fd2d41
Harris Brakmic <brakmic@gmail.com>
2023-07-21 21:43:46 +0200

    chore: reorder imports, make kc-admin-client default export

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,43 @@
-import KeycloakAdminClient from '@keycloak/keycloak-admin-client';
-import { ConnectionConfig } from '@keycloak/keycloak-admin-client/lib/client';
-import TokenProvider from '@keycloak/keycloak-admin-client';
-import { Users } from '@keycloak/keycloak-admin-client/lib/resources/users';
-import { UserStorageProvider } from '@keycloak/keycloak-admin-client/lib/resources/userStorageProvider';
-import { Groups } from '@keycloak/keycloak-admin-client/lib/resources/groups';
-import { Roles } from '@keycloak/keycloak-admin-client/lib/resources/roles';
-import { Clients } from '@keycloak/keycloak-admin-client/lib/resources/clients';
-import { Realms } from '@keycloak/keycloak-admin-client/lib/resources/realms';
-import { ClientScopes } from '@keycloak/keycloak-admin-client/lib/resources/clientScopes';
-import { ClientPolicies } from '@keycloak/keycloak-admin-client/lib/resources/clientPolicies';
-import { IdentityProviders } from '@keycloak/keycloak-admin-client/lib/resources/identityProviders';
-import { Components } from '@keycloak/keycloak-admin-client/lib/resources/components';
-import { ServerInfo } from '@keycloak/keycloak-admin-client/lib/resources/serverInfo';
-import { WhoAmI } from '@keycloak/keycloak-admin-client/lib/resources/whoAmI';
 import { AttackDetection } from '@keycloak/keycloak-admin-client/lib/resources/attackDetection';
-import { Sessions } from '@keycloak/keycloak-admin-client/lib/resources/sessions';
 import { AuthenticationManagement } from '@keycloak/keycloak-admin-client/lib/resources/authenticationManagement';
 import { Cache } from '@keycloak/keycloak-admin-client/lib/resources/cache';
+import { ClientPolicies } from '@keycloak/keycloak-admin-client/lib/resources/clientPolicies';
+import { ClientScopes } from '@keycloak/keycloak-admin-client/lib/resources/clientScopes';
+import { Clients } from '@keycloak/keycloak-admin-client/lib/resources/clients';
+import { ConnectionConfig } from '@keycloak/keycloak-admin-client/lib/client';
+import { Components } from '@keycloak/keycloak-admin-client/lib/resources/components';
+import { Groups } from '@keycloak/keycloak-admin-client/lib/resources/groups';
+import { IdentityProviders } from '@keycloak/keycloak-admin-client/lib/resources/identityProviders';
+import KeycloakAdminClient from '@keycloak/keycloak-admin-client';
+import { Realms } from '@keycloak/keycloak-admin-client/lib/resources/realms';
+import { Roles } from '@keycloak/keycloak-admin-client/lib/resources/roles';
+import { ServerInfo } from '@keycloak/keycloak-admin-client/lib/resources/serverInfo';
+import { Sessions } from '@keycloak/keycloak-admin-client/lib/resources/sessions';
+import TokenProvider from '@keycloak/keycloak-admin-client';
+import { UserStorageProvider } from '@keycloak/keycloak-admin-client/lib/resources/userStorageProvider';
+import { Users } from '@keycloak/keycloak-admin-client/lib/resources/users';
+import { WhoAmI } from '@keycloak/keycloak-admin-client/lib/resources/whoAmI';
+
+
+export default KeycloakAdminClient;
 
 export { 
-  KeycloakAdminClient,
+  AttackDetection,
+  Cache,
+  ClientPolicies,
+  ClientScopes,
+  Clients,
   ConnectionConfig,
+  Components,
+  Groups,
+  IdentityProviders,
+  Realms,
+  Roles,
+  ServerInfo,
+  Sessions,
   TokenProvider,
-  Users, 
-  UserStorageProvider, 
-  Groups, 
-  Roles, 
-  Clients, 
-  Realms, 
-  ClientScopes, 
-  ClientPolicies, 
-  IdentityProviders, 
-  Components, 
-  ServerInfo, 
-  WhoAmI, 
-  AttackDetection, 
-  Sessions, 
-  AuthenticationManagement, 
-  Cache 
+  UserStorageProvider,
+  Users,
+  WhoAmI,
+  AuthenticationManagement
 };
